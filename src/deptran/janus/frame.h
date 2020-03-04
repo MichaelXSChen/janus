497e6b74fe2d95e85574c7cc699aed54f3f7a832
Michael Xusheng Chen <michael.xschen@gmail.com>
2020-03-04 22:22:29 +0800

    understanding

--- src/deptran/janus/frame.h
+++ src/deptran/janus/frame.h
@@ -2,19 +2,39 @@
 
 #include "../frame.h"
 
+/*
+XSTODO: 
+
+1. who call these creates? Who save/own these created objs.
+2. Who call these objects' methods. 
+3. What are Executor for. 
+4. What are RPCServicies do.
+5. What are row, and tx.
+
+*/
 namespace janus {
 
 class JanusFrame : public Frame {
  public:
   JanusFrame(int mode = MODE_JANUS) : Frame(mode) {}
+  
+  //xs: always null ptr, whats that for?
   Executor *CreateExecutor(cmdid_t, Scheduler *sched) override;
+  
+  //xs: transaction coordinator,
+  //broadcastAccept to communicators.
+  //call acceptrACK to do after receive ACK. 
   Coordinator *CreateCoordinator(cooid_t coo_id,
                                  Config *config,
                                  int benchmark,
                                  ClientControlServiceImpl *ccsi,
                                  uint32_t id,
                                  TxnRegistry *txn_reg) override;
+  
+  //xs: What a participant do on receiving various requests.
   Scheduler *CreateScheduler() override;
+  
+  //xs: whats that for
   vector<rrr::Service *> CreateRpcServices(uint32_t site_id,
                                            Scheduler *dtxn_sched,
                                            rrr::PollMgr *poll_mgr,
